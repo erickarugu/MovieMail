a9958cfbbb7c25b33204d5fd7b53c0960d8ca29b
erickarugu32 <erickarugu32@gmail.com>
2021-03-04 12:56:19 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+var cookieParser = require('cookie-parser');
+var morgan = require('morgan');
+const exphbs = require("express-handlebars");
+const connectDB = require("./config/db");
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+// load config
+dotenv.config({
+  path: "./config/config.env",
+});
+
+// connect to db
+connectDB();
+
+var app = express();
+
+// logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Handle Bars
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs'
+}));
+app.set("view engine", ".hbs");
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on ${PORT}`));
+
---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+// @desc      Login/Landing page
+// @route     GET /
+router.get("/", (req, res) => {
+  res.render("register",{
+    layout: "main"
+  });
+});
+
+// @desc      Dashboard
+// @route     GET /dashboard
+router.get("/dashboard", async (req, res) => {
+  try {
+    res.render("dashboard");
+  } catch (error) {
+    res.render("error/500");
+  }
+});
+
+module.exports = router;
